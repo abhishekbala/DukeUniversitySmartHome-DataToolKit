0717d9ed2c1485672a550d724b489c4948f5376e
Raghav Saboo <saboo.raghav@gmail.com>
2014-12-04 16:56:57 -0500

    MySQL data connector

---
+++ getshdata.m
@@ -0,0 +1,38 @@
+%The data format preferences.
+setdbprefs('DataReturnFormat', 'numeric');
+setdbprefs('NullNumberRead', 'NaN');
+setdbprefs('NullStringRead', 'null');
+
+
+%ODBC connection to database.
+conn = database('Smart Home', 'student', '');
+ping(conn)
+
+%Set up cursor to read data using MySQL query
+mySQLquery = ['SELECT 	smart_home.timestamp'...
+    ' ,	smart_home.b7139_01_r_01_phase1'...
+    ' ,	smart_home.b7139_01_r_02_phase2'...
+    ' ,	smart_home.b7139_01_r_03_solar1'...
+    ' ,	smart_home.b7139_01_r_04_solar2'...
+    ' ,	smart_home.b7139_03_r_01_refrig'...
+    ' ,	smart_home.b7139_06_r_01_irrigation'...
+    ' FROM 	energydata.smart_home '...
+    ' WHERE 	smart_home.timestamp >= 1417628969'...
+    ' ORDER BY 	smart_home.timestamp DESC '];
+
+curs = exec(conn,mySQLquery);
+
+%Fetch data based on cursor used above
+curs = fetch(curs,10);
+
+%Store data locally on Matlab
+shData = curs.Data;
+
+%Close cursor
+close(curs);
+
+%Close database connection.
+close(conn);
+
+%Clear variables
+clear
