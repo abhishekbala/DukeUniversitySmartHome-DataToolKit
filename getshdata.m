f47d3423620fcd25a489b112e37388aded73dc36
Abhishek Balakrishnan <abhishek.bend@gmail.com>
2014-12-04 20:21:46 -0500

    Live streaming code is fully functional

--- getshdata.m
+++ getshdata.m
@@ -1,29 +1,6 @@
-%The data format preferences.
-setdbprefs('DataReturnFormat', 'numeric');
-setdbprefs('NullNumberRead', 'NaN');
-setdbprefs('NullStringRead', 'null');
+function shData = getshdata(conn, mySQLquery)
 
-
-%ODBC connection to database.
-conn = database('Smart Home', 'student', '');
-ping(conn)
-
-%Set up cursor to read data using MySQL query
-% ' ,	smart_home.b7139_01_r_02_phase2'...
-%    ' ,	smart_home.b7139_01_r_03_solar1'...
-%    ' ,	smart_home.b7139_01_r_04_solar2'...
-%    ' ,	smart_home.b7139_03_r_01_refrig'...
-%    ' ,	smart_home.b7139_06_r_01_irrigation'...
-
-mySQLquery = ['SELECT 	smart_home.timestamp'...
-    ' ,	smart_home.b7139_01_r_01_phase1'...
-    ' , smart_home.b7139_01_r_02_phase2'...
-    ' FROM 	energydata.smart_home '...
-    ' WHERE 	smart_home.timestamp >= 1417735228'...
-    ' ORDER BY 	smart_home.timestamp DESC'];
-%tic;
 curs = exec(conn,mySQLquery);
-%t = toc;
 
 %Fetch data based on cursor used above
 curs = fetch(curs,200);
@@ -35,7 +12,9 @@ shData = curs.Data;
 close(curs);
 
 %Close database connection.
-% close(conn);
+%close(conn);
 
 %Clear variables
 clear conn curs
+
+end
