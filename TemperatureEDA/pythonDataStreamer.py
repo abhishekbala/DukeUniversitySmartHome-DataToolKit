b7b26bdf018eeaa65af1fe1d66c226b9637c250b
Raghav Saboo <saboo.raghav@gmail.com>
2015-02-20 22:19:46 -0500

    Auto updating csv with Smart Home data

--- TemperatureEDA/pythonDataStreamer.py
+++ TemperatureEDA/pythonDataStreamer.py
@@ -4,26 +4,34 @@ Created on Sun Jan 25 17:09:49 2015
 
 @author: Raghav Saboo
 """
+from datetime import datetime, timedelta
+import time
 import csv
 import pymysql.cursors
 
-connection=pymysql.connect(
-    host='67.159.81.86',user='student',
-    passwd='bassconnex',db='energydata',
-    cursorclass = pymysql.cursors.SSCursor)
-cursor=connection.cursor()
+for _ in range(30):
+    # There is a 51 second lag in the MySQL data.
+    # The last 5 minutes worth of data is to be loaded every 5 minutes.
+    unixTime = str(int(time.time())-352) 
+    
+    connection=pymysql.connect(
+        host='67.159.81.86',user='student',
+        passwd='bassconnex',db='energydata',
+        cursorclass = pymysql.cursors.SSCursor)
+    cursor=connection.cursor()
+    
+    cursor.execute("SELECT smarthome.timestamp,	smarthome.b7139_01_r_01_phase1 \
+    , smarthome.b7139_01_r_02_phase2 FROM energydata.smarthome \
+    WHERE smarthome.timestamp =" +unixTime+ " \
+    ORDER BY smarthome.timestamp ASC")
+    
+    shData = [row for row in cursor]
+    
+    with open("shData.csv",'a') as csv_out:
+        csvwriter = csv.writer(csv_out,delimiter=',',dialect='excel')
+        csvwriter.writerows(shData)
+    
+    time.sleep(1)
 
-cursor.execute("SELECT smarthome.timestamp,	smarthome.b7139_01_r_01_phase1 \
-, smarthome.b7139_01_r_02_phase2 FROM energydata.smarthome \
-ORDER BY smarthome.timestamp DESC")
-
-csv_out = open("shData.csv",'wb')
-csvwriter = csv.writer(csv_out)
-
-shData = [row for row in cursor]
-csvwriter.writerows(shData)
-
-#for row in cursor:
-#    print(row)
-#cursor.close()
-#connection.close()
+cursor.close()
+connection.close()
