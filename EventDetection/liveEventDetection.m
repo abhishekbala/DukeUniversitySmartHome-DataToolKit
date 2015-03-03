57ecab97d72a35103ec8c1aa8162b78fd7ccaba1
Raghav Saboo <saboo.raghav@gmail.com>
2015-03-03 10:54:36 -0500

    Data from SH

--- EventDetection/liveEventDetection.m
+++ EventDetection/liveEventDetection.m
@@ -3,23 +3,19 @@ function liveEventDetection()
     liveData = csvread('../dataCollectors/shData.csv');
     endRow = length(liveData(:,1));
     
+    % Keep window (for plotting) at 300seconds (can be changed to any value
+    % upto 1800).
+    window = 1800;
+    times = liveData(endRow-window:endRow,1);
+    aggregatePower = sum(liveData(endRow-window:endRow,2:3),2);
+    
     % Original data
+    %refresh
     %figure(1);
     %plot(times, aggregatePower);
     
     % Event Detection
     FastGLR_forEventDetectionMetrics(aggregatePower,40,30,-10,3,0,6);
-    % Keep window (for plotting) at 300seconds (can be changed to any value
-    % upto 1800).
-    window = 300;
-    times = liveData(endRow-window:endRow,1);
-    aggregatePower = sum(liveData(endRow-window:endRow,2:3),2);
-    
-    % Plot
-    refresh
-    figure(1);
-    plot(times,aggregatePower);
-    % Event Detection
     
     % 1 second pause
     pause(1)
