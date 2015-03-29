bb994e1290d9f01e1afe84fc125cd33b9f5d5e9c
Raghav Saboo <saboo.raghav@gmail.com>
2015-03-29 17:48:54 -0400

    Metrics

--- EventDetection/AccuracyMetrics/Metrics.m
+++ EventDetection/AccuracyMetrics/Metrics.m
@@ -1,7 +1,11 @@
-function [ output_args ] = Metrics( input_args )
+function [ output_args ] = Metrics( classifiedData, testTruthData, uniqueClasses, classNames )
 %METRICS Summary of this function goes here
 %   Detailed explanation goes here
+%   uniqueClasses <-- numeric i.e. 5
+%   classNames <-- array (5x1) i.e. {'Class 1', 'Class 2'...}
 
+% Load a binary truth dataset
 
+prtScoreConfusionMatrix(classifiedData,testTruthData,uniqueClasses,classNames);
 end
 
