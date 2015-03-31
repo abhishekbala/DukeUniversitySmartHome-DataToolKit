c18529ae73e005ef6def6ce2a67839e342f49678
Raghav Saboo <saboo.raghav@gmail.com>
2015-03-31 15:17:38 -0400

    redone

--- EventDetection/AccuracyMetrics/Test Vectors/1 Day 2 Features/concatenateVectors.m
+++ EventDetection/AccuracyMetrics/Test Vectors/1 Day 2 Features/concatenateVectors.m
@@ -14,30 +14,16 @@ OFF.data = OFFdcsID';
 dummy = prtDataSetClass;
 truthON = prtDataSetClass;
 load('h10p.mat')
-dummy.data = [h10onTruth*3]';
-dummy.targets = dummy.data;
-truthON = catObservations(truthON, dummy);
-
-dummy = prtDataSetClass;
 load('refrigerator.mat')
-dummy.data = [refOnTruth*1]';
-dummy.targets = dummy.data;
-truthON = catObservations(truthON, dummy);
-
-dummy = prtDataSetClass;
 load('hotbox.mat')
-dummy.data = [hotOnTruth*5]';
-dummy.targets = dummy.data;
-truthON = catObservations(truthON, dummy);
-
-dummy = prtDataSetClass;
 load('HVAC1.mat')
-dummy.data = [HVAC1onTruth*7]';
+load('HVAC2.mat')
+h10onTruth = [h10onTruth*3]';
+refOnTruth= [refOnTruth*1]';
+hotOnTruth = [hotOnTruth*5]';
+HVAC1onTruth = [HVAC1onTruth*7]';
+HVAC2onTruth = [HVAC2onTruth*9]';
+onTruths = h10onTruth + refOnTruth + hotOnTruth + HVAC1onTruth + HVAC2onTruth;
+dummy.data = onTruths;
 dummy.targets = dummy.data;
 truthON = catObservations(truthON, dummy);
-
-dummy = prtDataSetClass;
-load('HVAC2.mat')
-dummy.data = [HVAC2onTruth*9]';
-dummy.targets = dummy.data;
-truthON = catObservations(truthON, dummy)
