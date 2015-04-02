9c931ad0f14eb0fd534a186f2bc75feb84c73bb3
Raghav Saboo <saboo.raghav@gmail.com>
2015-04-01 23:12:50 -0400

    working on confusionMatrix

--- EventDetection/AccuracyMetrics/Test Vectors/1 Day 2 Features/concatenateVectors.m
+++ EventDetection/AccuracyMetrics/Test Vectors/1 Day 2 Features/concatenateVectors.m
@@ -23,6 +23,14 @@ h10onTruth = [h10onTruth*2]';
 hotOnTruth = [hotOnTruth*3]';
 HVAC1onTruth = [HVAC1onTruth*4]';
 HVAC2onTruth = [HVAC2onTruth*5]';
+
+a = find(refOnTruth == 0); b = find(h10onTruth == 0); c = find(hotOnTruth == 0);
+d = find(HVAC1onTruth == 0); e = find(HVAC2onTruth == 0);
+
+for i = 1:length(refOnTruth)
+    if isempty(find(a == i));
+        
+
 onTruths = h10onTruth + refOnTruth + hotOnTruth + HVAC1onTruth + HVAC2onTruth;
 dummy.data = onTruths;
 dummy.targets = dummy.data;
