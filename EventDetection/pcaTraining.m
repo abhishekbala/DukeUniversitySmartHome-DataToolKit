eb0bc1f5e7730b337a902684c4c30600c6f1a20c
Raghav Saboo <saboo.raghav@gmail.com>
2015-04-08 01:23:31 -0400

    Yup it is a push with a new feature space.

--- EventDetection/pcaTraining.m
+++ EventDetection/pcaTraining.m
@@ -1,58 +1,19 @@
-function pcaTraining(data, on, off)
+function [onMatrix, offMatrix] = pcaTraining(data, on, off)
 %% Extract 61 points around each ON Event
-onMatrix = zeros(length(n), 61);
-offMatrix = zeros(length(n), 61);
-
+onIndex = find(on);
+offIndex = find(off);
+onMatrix = zeros(length(onIndex), 61);
+offMatrix = zeros(length(offIndex), 61);
 %% Normalize on/off windows
-for i = 1:length(on)
-    onMean = mean(data(on(i)-30:on(i)+30));
-    onStd = std(data(on(i)-30:on(i)+30));
-    onMatrix(i, :) = (data(on(i)-30:on(i)+30) - onMean) / onStd;
+for i = 1:length(onIndex)
+    onMean = mean(data(onIndex(1,i)-30:onIndex(1,i)+30,1));
+    onStd = std(data(onIndex(1,i)-30:onIndex(1,i)+30,1));
+    onMatrix(i, :) = ((data(onIndex(1,i)-30:onIndex(1,i)+30,1) - onMean))'; % / onStd)';
 end
-
-for i = 1:length(off)
-    offMean = mean(data(off(i)-30:off(i)+30));
-    offStd = std(data(off(i)-30:off(i)+30));
-    offMatrix(i, :) = (data(off(i)-30:off(i)+30) - offMean) / offStd;
+onMatrix;
+for i = 1:length(offIndex)
+    offMean = mean(data(offIndex(1,i)-30:offIndex(1,i)+30,1));
+    offStd = std(data(offIndex(1,i)-30:offIndex(1,i)+30,1));
+    offMatrix(i, :) = ((data(offIndex(1,i)-30:offIndex(1,i)+30,1) - offMean))'; % / offStd)';
 end
-
-%% PCA Training
-
-
-%% PCA training to be run then
-%pcaTraining(refrigerator,onRef,offRef)
-onFeaturesMatrix = [];
-offFeaturesMatrix = [];
-onFeaturesMatrix = [onFeaturesMatrix; onMatrix];
-offFeaturesMatrix = [offFeaturesMatrix; offMatrix];
-[n,m] = size(onMatrix)
-[p,q] = size(offMatrix)
-onTargets = [];
-offTargets = [];
-onTargets = [onTargets; ones(n,1)];
-offTargets = [offTargets; ones(n,1)];
-%hotbox
-onFeaturesMatrix = [onFeaturesMatrix; onMatrix];
-offFeaturesMatrix = [offFeaturesMatrix; offMatrix];
-onTargets = [onTargets; 2*ones(n,1)];
-offTargets = [offTargets; 2*ones(n,1)];
-%HVAC1
-onFeaturesMatrix = [onFeaturesMatrix; onMatrix];
-offFeaturesMatrix = [offFeaturesMatrix; offMatrix];
-onTargets = [onTargets; 3*ones(n,1)];
-offTargets = [offTargets; 3*ones(n,1)];
-%HVAC2
-onFeaturesMatrix = [onFeaturesMatrix; onMatrix];
-offFeaturesMatrix = [offFeaturesMatrix; offMatrix];
-onTargets = [onTargets; 4*ones(n,1)];
-offTargets = [offTargets; 4*ones(n,1)];
-
-onFeatureSet = prtDataSetClass;
-offFeatureSet = prtDataSetClass;
-
-onFeatureSet.data = onFeaturesMatrix;
-onFeatureSet.targets = onTargets;
-
-offFeatureSet.data = offFeaturesMatrix;
-offFeatureSet.targets = offTargets;
-end 
+offMatrix;
