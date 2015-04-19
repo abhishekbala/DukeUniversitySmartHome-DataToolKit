554c1d77f2f20b3e82f99f34c0cfe6807dc6f823
unknown <abl17@duke.edu>
2015-04-19 01:34:14 -0400

    Added to DisaggregationOutput2.m

---
+++ EventDetection/DisaggregationOutput2.m
@@ -0,0 +1,57 @@
+function DisaggregationOutput2()
+%DISAGGREGATIONOUTPUT2 Summary of this function goes here
+%   Detailed explanation goes here
+
+%% Make Initial CSV File:
+csvwrite('DisaggregatedPower.csv', []);
+
+%% Get the first time vector
+firstTimeData = importdata('../dataCollectors/shData.csv');
+[numRows, ~] = size(firstTimeData);
+functionPointers = [0, 0, 0, 0];
+
+for i = 1:numRows
+    % Assume that the functionPointers are [0, 0, 0, 0] ==> all appliances
+    % are off:
+    
+    % This code updates functionPointers, showing whether each particular
+    % appliance is on or off:
+    %     if(i ~= 1)
+    %         disaggregatedData = importdata('DisaggregatedPower.csv');
+    %         [numRowsDisaggregatedPower, numColsDisaggregatedPower] = size(disaggregatedData);
+    %         appliancePowers = disaggregatedData(numRowsDisaggregatedPower, :);
+    %         appliancePowers = appliancePowers(2:length(appliancePowers));
+    %         functionPointers(appliancePowers ~= 0) = 1;
+    %     end
+    
+    currentTime = firstTimeData(i,1);
+    disagMatrix = firstTimeData(:,i);
+    
+    [appliancePowerOutputs, functionPointers] = DisaggregationOutput(currentTime, disagMatrix, functionPointers);
+    
+    %% CSV Write:
+    dlmwrite('DisaggregatedPower.csv', appliancePowerOutputs, '-append', 'newline', 'pc');
+end
+
+%% Continuous Stream:
+    disaggregatedData = importdata('DisaggregatedPower.csv');
+    [numRowsDisaggregatedPower, ~] = size(disaggregatedData);
+    appliancePowers = disaggregatedData(numRowsDisaggregatedPower, :);
+    appliancePowers = appliancePowers(2:length(appliancePowers));
+    functionPointers(appliancePowers ~= 0) = 1;
+
+while(1)
+    liveData = importdata('../dataCollectors/shData.csv');
+    [numRows, ~] = size(liveData);
+    
+    currentTime = liveData(i,1);
+    disagMatrix = liveData(:,i);    
+    
+    [appliancePowerOutputs, functionPointers] = DisaggregationOutput(currentTime, disagMatrix, functionPointers);
+    
+        %% CSV Write:
+    dlmwrite('DisaggregatedPower.csv', appliancePowerOutputs, '-append', 'newline', 'pc');
+end
+
+end
+
