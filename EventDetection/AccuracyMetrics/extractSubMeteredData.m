f842d251a9e8ee1c4b7de88c1e376fffb09f556c
Raghav Saboo <saboo.raghav@gmail.com>
2015-04-23 16:45:13 -0400

    Revert "Revert "Raghav's sections cleaned up""
    This reverts commit 9e967efb1b9fff12cf93f130645f7a82c5b7367e.

--- EventDetection/AccuracyMetrics/extractSubMeteredData.m
+++
@@ -1,9 +0,0 @@
-function [time, refrigerator, h10p, hotbox, HVAC1, HVAC2, agg] = extractSubMeteredData()
-%% COMMENTS TO BE UPDATED
-
-shMar2015 = importdata('shMar2015(1Day).csv');
-
-time = shMar2015(:,1); refrigerator = shMar2015(:,14); h10p = shMar2015(:,12);
-hotbox = shMar2015(:,13); HVAC1 = shMar2015(:,6) + shMar2015(:,8);
-HVAC2 = shMar2015(:,7) + shMar2015(:,9);
-agg = sum(shMar2015(:,2:3),2) - sum(shMar2015(:,4:5),2);
---
+++ EventDetection/Feature Extraction and Metrics/GLR_EventDetection.m
@@ -0,0 +1,209 @@
+function [eventsDetected_on, eventsDetected_off, eventsDetected] = GLR_EventDetection(data, w_BeforeAfterLength, w_GLRLength, v_Threshold, SignalNoiseRatio, preProcessOption, GLRSmoothingOption, ZScoreValue)
+%% Parameters
+% Output: eventsDetected => Binary array of when events turn on. 1 = event
+% and 0 = non-event
+%
+% Data => inputted data
+% w_BeforeAfterLength ==> Represents the window length for calculating
+% statistics. Remember, in general, a window length of > 30 gets closer and
+% closer to normally distributed.
+%                   DEFAULT: 40
+% w_GLRLength ==> Increase or decrease window which we calculate GLR.
+%                   DEFAULT: 30
+% SignalNoiseRatio ==> Determines how strong additive wgn is.
+%                   DEFAULT 1
+% preProcessOption ==> Values 0 to 4.
+%                    DEFAULT 3: smoothing + additive
+% GLRSmoothingOption ==> Values 0 to 4.
+%                   DEFAULT 0: No smoothing
+% ZScoreValue ==> Threshold of transitional importance.
+%                   DEFAULT 4: 99.99% Noise elminiation
+
+% Timing
+%clock
+
+if(size(data, 1) == 1 && size(data, 2) ~= 1) % Making sure data is in right format
+    data = data';
+end
+
+if(nargin == 1)
+    w_BeforeAfterLength = 40;
+    w_GLRLength = 30;
+    v_Threshold = 25;
+    SignalNoiseRatio = 1;
+    preProcessOption = 3;
+    GLRSmoothingOption = 0;
+    ZScoreValue = 4;
+end
+
+%% DETECT EVENTS USING GENERALIZED LIKELIHOOD RATIO
+
+%% Defining Variables
+wa = w_BeforeAfterLength; % Window of the next w_BeforeAfterLength points
+wb = w_BeforeAfterLength; % Window of the previous w_BeforeAfterLength points
+current = wb; % Initialize the point in which the pointer starts at
+startIndex = current; % Initialize the start index at the w_BeforeAfterLength data point
+wl = w_GLRLength; % Window in which we calculate the GLR
+
+vt = v_Threshold; % Voting threshold
+
+
+dataLength = length(data); % Length of the data
+l = zeros(1, dataLength); % GLR values for all the data points in set
+s = zeros(1, dataLength); % Generalized voting statistic for data points
+v = zeros(1, dataLength); % Votes for each particular data point
+
+v_onEvents = zeros(1, dataLength); % Votes for on events specifically
+v_offEvents = zeros(1, dataLength); % Votes for off events specifically
+
+SNR = SignalNoiseRatio; % Signal to Noise Ratio
+
+%% Preprocessing the Data:
+%%%%%% Testing Cases: Include adding white gaussian noise, ect. 1 OF 5
+%%%%%% OPTIONS!!!!!
+switch preProcessOption
+    case 0 % No preprocessing
+        modifiedData = data;
+    case 1 % Adding only white gaussian noise
+        modifiedData = awgn(data, SNR);
+    case 2 % Only smoothing data
+        modifiedData = smooth(data, 'sgolay');
+    case 3 % First adding white gaussian noise, then smoothing
+        modifiedData = smooth(awgn(data, SNR), 'sgolay');
+    case 4 % First smoothing the data then adding additive white gaussian noise
+        modifiedData = awgn(smooth(data, 'sgolay', 4), SNR);
+end
+
+myDataStats = zeros(4, length(modifiedData));
+% For each point in the data set, give the values of:
+% Row 1: Mean of wa
+% Row 2: Mean of wb
+% Row 3: St.D of wa
+% Row 4: St.D of wb
+
+for i = (1 + wb):(length(modifiedData)-wa)
+    xn = modifiedData(i);
+% % % % % %     meana = mean(modifiedData((i+1):(i+wa)));
+% % % % % %     meanb = mean(modifiedData((i-wb):(i-1)));
+    meana = mean(data((i+1):(i+wa)));
+    meanb = mean(data((i-wb):(i-1)));
+    sigmaa = std(modifiedData((i+1):(i+wa)));
+    sigmab = std(modifiedData((i-wb):(i-1)));
+    
+    myDataStats(1, i) = meana;
+    myDataStats(2, i) = meanb;
+    myDataStats(3, i) = sigmaa;
+    myDataStats(4, i) = sigmab;
+    
+    %    l(i) = - (xn - meana)^2 / (2*sigmaa) + (xn - meanb)^2 / (2*sigmab);
+    %     This code is technically incorrect: Should be the BELOW:
+    
+    l(i) = - (xn - meana).^2 / (2.*sigmaa.^2) + (xn - meanb).^2 / (2.*sigmab.^2 );
+    
+end
+%%%%% Smoothing Additive (May Improve Performance)
+switch GLRSmoothingOption
+    case 0
+        l = 1.*l; % Do Nothing
+    case 1
+        smooth(l, 'moving');
+    case 2
+        smooth(l, 'sgolay');
+    case 3
+        smooth(l, 'rloess');
+    case 4
+        smooth(l, 'rlowess');
+end
+
+while startIndex + wl - 1 + wa < length(data) % As long as the GLR window + window of after doesn't exceed size of our data set.
+    startIndex = startIndex + 1;
+    endIndex = startIndex + wl - 1;
+    
+    for wi = 1 : wl
+        current = startIndex + wi;
+        s(current) = sum(l(current : endIndex)); % A weighting of different GLR values to emphasize earlier values
+    end
+    
+    smaxi = find(s(startIndex : endIndex) == max(s(startIndex : endIndex))); % We want to find the maximum stats value for each window
+    smaxi = startIndex - 1 + smaxi(1);
+    
+    %% Transitional Priority:
+    % This uses statistical confidence intervals to eliminate votes in
+    % which the data point is surrounded by 'noise'
+    
+    Za = (modifiedData(smaxi) -  myDataStats(1, smaxi+5))./myDataStats(3, smaxi+5);
+    Zb = (modifiedData(smaxi) -  myDataStats(2, smaxi-5))./myDataStats(4, smaxi-5);
+    
+    if(abs(Za) < ZScoreValue && abs(Zb) < ZScoreValue) % 99.99% Interval
+        v(smaxi) = v(smaxi) + 0;
+    else
+        v(smaxi) = v(smaxi) + 1;
+        % % % %         if(Za > 0)
+        % % % %             v_offEvents(smaxi) = v_offEvents(smaxi) + 1;
+        % % % %         elseif(Za < 0)
+        % % % %             v_onEvents(smaxi) = v_onEvents(smaxi) + 1;
+        % % % %         end
+        %%%%%%%%%%%%%%%%%%%%%% INSTEAD OF THE ABOVE CONDITION: Maybe actually
+        %%%%%%%%%%%%%%%%%%%%%% comparing values after vs before values would give
+        %%%%%%%%%%%%%%%%%%%%%% us a good representation of on vs off.
+        if(modifiedData(smaxi+4) - modifiedData(smaxi - 2) < 0)
+            v_offEvents(smaxi) = v_offEvents(smaxi) + 1;
+        elseif(modifiedData(smaxi+4) - modifiedData(smaxi - 2) >= 0)
+            v_onEvents(smaxi) = v_onEvents(smaxi) + 1;
+        end
+    end
+    
+    
+    
+end
+
+%% Thresholding the vote counts. Normalizing vote count to data and plotting:
+
+v(v < vt) = 0;
+v(v >= vt) = 1;
+v_scaled = v.*(data');
+v_scaled(v_scaled == 0) = NaN;
+
+v_onEvents(v_onEvents < vt) = 0;
+v_onEvents(v_onEvents >= vt) = 1;
+v_onEvents_scaled = v_onEvents.*(data');
+v_onEvents_scaled(v_onEvents_scaled == 0) = NaN;
+
+v_offEvents(v_offEvents < vt) = 0;
+v_offEvents(v_offEvents >= vt) = 1;
+v_offEvents_scaled = v_offEvents.*(data');
+v_offEvents_scaled(v_offEvents_scaled == 0) = NaN;
+
+eventsDetected = v;
+eventsDetected_on = v_onEvents;
+eventsDetected_off = v_offEvents;
+
+%% End Timing:
+%clock
+
+%% Plotting Relevant Features
+clf; % Clear Relevant Figures
+
+figure(1);
+hold on;
+plot(data);
+plot(v_onEvents_scaled, 'g*', 'linewidth', 4);
+plot(v_offEvents_scaled, 'r*', 'linewidth', 4);
+hold off;
+title('Events detected for BLUED Data Set: Aggregate Data');
+xlabel('Time Series Values (s)');
+ylabel('Power Values (W)');
+legend('Data', 'On Events', 'Off Events');
+
+% figure(2);
+% hold on;
+% plot(data);
+% plot(v_scaled, 'ro', 'linewidth', 2);
+% hold off;
+% title('Events detected');
+% xlabel('Time Series Values (s)');
+% ylabel('Power Values (W)');
+% legend('Data', 'Events');
+
+end
+
